d3390e1a397f4f2913024e4050f38ff0cceac45d
Vinicius Nascimento Ribeiro <vinicius.ribeiro44@fatec.sp.gov.br>
2024-08-24 18:42:39 -0300

    chore: Add header, nav, main, aside, and footer components

---
+++ src/components/Aside.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Aside.css';
+
+const Aside = () => {
+  return (
+    <aside className="aside">
+      <h2>Aside Section</h2>
+      <p>This is some additional information or links.</p>
+    </aside>
+  );
+};
+
+export default Aside;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Main.css';
+
+const Main = () => {
+  return (
+    <main className="main">
+      <section>
+        <h1>Welcome to the main content area</h1>
+        <p>This is where the main content goes.</p>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
